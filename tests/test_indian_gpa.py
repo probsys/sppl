72b8a516264e6d34071d7ee0eec3f69dfaa49a5b
Feras A. Saad <fsaad@mit.edu>
2020-02-24 20:46:01 -0500

    Implement IfElse combinator (see notes for known issue; closes #10).

--- tests/test_indian_gpa.py
+++ tests/test_indian_gpa.py
@@ -8,21 +8,21 @@ from spn.distributions import Uniform
 from spn.math_util import allclose
 from spn.transforms import Identity
 
+from spn.combinators import IfElse
 from spn.spn import ExposedSumSPN
 from spn.spn import NominalDistribution
 
+N = Identity('N')
+P = Identity('P')
 GPA = Identity('GPA')
 
-model_implicit = \
+model_no_latents = \
         0.5 * ( # American student
             0.99 * Uniform(GPA, loc=0, scale=4) | \
             0.01 * Atomic(GPA, loc=4)) | \
         0.5 * ( # Indian student
             0.99 * Uniform(GPA, loc=0, scale=10) | \
             0.01 * Atomic(GPA, loc=10))
-
-N = Identity('N')
-P = Identity('P')
 
 nationality = NominalDistribution(N, {'India': 0.5, 'USA': 0.5})
 perfect = NominalDistribution(P, {'Imperfect': 0.99, 'Perfect': 0.01})
@@ -45,7 +45,81 @@ model_exposed = ExposedSumSPN(
             })},
     )
 
-@pytest.mark.parametrize('model', [model_implicit, model_exposed])
+model_ifelse_exhuastive = IfElse(nationality & perfect,
+    [(N << {'India'}) & (P << {'Imperfect'}),
+        Uniform(GPA, loc=0, scale=10)
+    ],
+    [(N << {'India'}) & (P << {'Perfect'}),
+        Atomic(GPA, loc=10)
+    ],
+    [(N << {'USA'}) & (P << {'Imperfect'}),
+        Uniform(GPA, loc=0, scale=4)
+    ],
+    [(N << {'USA'}) & (P << {'Perfect'}),
+        Atomic(GPA, loc=4)
+    ])
+
+model_ifelse_nested = IfElse(nationality,
+    [(N << {'India'}),
+        IfElse(perfect,
+            [(P << {'Imperfect'}), Uniform(GPA, loc=0, scale=10)],
+            [True, Atomic(GPA, loc=10)])
+    ],
+    [True,
+        IfElse(perfect,
+            [(P << {'Imperfect'}), Uniform(GPA, loc=0, scale=4)],
+            [True, Atomic(GPA, loc=4)])
+    ])
+
+# Known issue #1
+# The non-exhaustive model, that uses True for the final statement,
+# is slow, because the compiled event is very complicated.
+# This may be due to the fact that we do not know how to simplify
+# complements of Nominal variables well, in absence of the explicit
+# universe over which the variables take their values.  One solution
+# is to store the possible nominal values in a global environment
+# when compiling the program, and allow EventFiniteSet to take in
+# the finite support of the nominal variate.
+model_ifelse_non_exhuastive = IfElse(nationality & perfect,
+    [(N << {'India'}) & (P << {'Imperfect'}),
+        Uniform(GPA, loc=0, scale=10)
+    ],
+    [(N << {'India'}) & (P << {'Perfect'}),
+        Atomic(GPA, loc=10)
+    ],
+    [(N << {'USA'}) & (P << {'Imperfect'}),
+        Uniform(GPA, loc=0, scale=4)
+    ],
+    [True,
+        Atomic(GPA, loc=4)
+    ])
+
+# Known issue #2
+# The nested model with repeated variables raises an error,
+# because IfElse takes the product of the current SPN with the
+# child branch.  Instead we need to implement the IfElse combinators
+# as specified in the SOS semantics from README, which finds the child
+# SPN recursively from the sub-branch by applying the sequence rule
+# to the root SPN with the branch SPNs.  This implementation will need
+# an interpreter of the syntax and may not be possible to implement
+# as vanilla Python.
+#
+# model_ifelse_nested_repeat = IfElse(nationality & perfect,
+#     [(N << {'India'}),
+#         IfElse(nationality & perfect,
+#             [(P << {'Imperfect'}), Uniform(GPA, loc=0, scale=10)],
+#             [True, Atomic(GPA, loc=10)])
+#     ],
+#     [True,
+#         IfElse(nationality & perfect,
+#             [(P << {'Imperfect'}), Uniform(GPA, loc=0, scale=4)],
+#             [True, Atomic(GPA, loc=4)])
+#     ])
+
+@pytest.mark.parametrize('model', [model_no_latents,
+    model_exposed,
+    model_ifelse_exhuastive,
+    model_ifelse_nested])
 def test_prior(model):
     assert allclose(model.prob(GPA << {10}), 0.5*0.01)
     assert allclose(model.prob(GPA << {4}), 0.5*0.01)
@@ -71,12 +145,12 @@ def test_prior(model):
     assert allclose(model.prob(((2 <= GPA) < 4) & (7 < GPA)), 0)
 
 def test_condition():
-    model_condition = model_implicit.condition(GPA << {4} | GPA << {10})
+    model_condition = model_no_latents.condition(GPA << {4} | GPA << {10})
     assert len(model_condition.children) == 2
     assert model_condition.children[0].support == {4}
     assert model_condition.children[1].support == {10}
 
-    model_condition = model_implicit.condition((0 < GPA < 4))
+    model_condition = model_no_latents.condition((0 < GPA < 4))
     assert len(model_condition.children) == 2
     assert model_condition.children[0].support \
         == model_condition.children[1].support
