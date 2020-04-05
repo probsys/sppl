35d24b7aa9713456d4e06352639f31718d138782
Feras A. Saad <fsaad@mit.edu>
2020-04-05 18:29:54 -0400

    Pin PyPI versions in requirements.txt and add dependencies from apt.

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+# Libraries to install pygraphviz from PyPI.
+# Tested on Ubuntu 18.04, other platforms may vary.
+apt-get install \
+    graphviz \
+    libgraphviz-dev \
+    pkg-config \
+    python3-dev
