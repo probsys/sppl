28db61d6aee7e1b20b8cdb3002061d741c925cad
Feras A. Saad <fsaad@mit.edu>
2020-04-10 15:38:10 -0400

    Use Fraction weights when NominalDistribution calls flip.

--- src/spn.py
+++ src/spn.py
@@ -781,8 +781,8 @@ class NominalDistribution(LeafSPN):
         self.dist = {NominalValue(x): Fraction(w) for x, w in dist.items()}
         # Derived attributes.
         self.support = NominalSet(*dist.keys())
-        self.outcomes = list(self.dist)
-        self.weights = [float(x) for x in self.dist.values()]
+        self.outcomes = list(self.dist.keys())
+        self.weights = list(self.dist.values())
         assert allclose(float(sum(self.weights)),  1)
 
     def logpdf(self, x):
