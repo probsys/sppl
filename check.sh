e5fc805824a6b6742e75d14f62a84b50228f31f6
Feras A. Saad <fsaad@mit.edu>
2020-10-04 14:20:03 -0400

    Update check.sh script documentation.

--- check.sh
+++ check.sh
@@ -12,21 +12,18 @@ root=`cd -- "$(dirname -- "$0")" && pwd`
     rm -rf build
     "$PYTHON" setup.py build
     if [ $# -eq 0 ] || [ ${1} = 'crash' ]; then
-        # By default run all tests.
-        # Any test which uses this flag should end with __ci_() which
-        # activates integration testing code path. If --integration is
-        # not specified then a __ci_() test will either run as a crash test
-        # or not run at all. (Use git grep '__ci_' to find these tests.)
+        # (Default) Run tests not marked __ci__
         ./pythenv.sh "$PYTHON" -m pytest -k 'not __ci_' --pyargs sppl
     elif [ ${1} = 'ci' ]; then
+        # Run all tests under tests/
         ./pythenv.sh "$PYTHON" -m pytest --pyargs sppl
     elif [ ${1} = 'coverage' ]; then
+        # Generate coverage report.
         ./pythenv.sh coverage run --source=build/ -m pytest --pyargs sppl
         coverage html
         coverage report
     elif [ ${1} = 'examples' ]; then
         # Run the .ipynb notebooks under examples/
-        # Requires 'magics' dependencies listed in setup.py
         for x in $(ls examples/*.ipynb); do
             rm -rf ${x%%.ipynb}.html
             ./pythenv.sh jupyter nbconvert --execute --to html ${x};
