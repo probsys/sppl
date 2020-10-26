82e775b82a1f21b8119b5ee4187bce0f4537248d
Feras A. Saad <fsaad@mit.edu>
2020-10-26 15:38:26 -0400

    Add instructions for magics in the Installation section.

--- requirements.sh
+++ requirements.sh
@@ -2,8 +2,4 @@
 # Libraries to install pygraphviz from PyPI.
 # Tested on Ubuntu 18.04, other platforms may vary.
 
-apt-get -y install \
-    graphviz \
-    libgraphviz-dev \
-    pkg-config \
-    python3-dev
+apt-get -y install graphviz libgraphviz-dev
