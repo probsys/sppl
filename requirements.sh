34a34b3d8683665687f1f483ffe4a6ab0ba11310
Feras A. Saad <fsaad@mit.edu>
2021-06-22 12:19:43 -0400

    Update requirements.sh.

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Libraries to install pygraphviz from PyPI.
-# Tested on Ubuntu 18.04, other platforms may vary.
+# Tested on Ubuntu 18.04+, other platforms may vary.
 
-apt-get -y install graphviz libgraphviz-dev
+apt-get -y install python3-dev graphviz libgraphviz-dev
