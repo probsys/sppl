3384f142ddcdb3ad3910e0e89bce272a9398945b
McCoy Becker <mccoybecker@gmail.com>
2022-01-25 17:16:44 -0500

    Bump scipy compat.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = {
     'src' : [
         'astunparse==1.6.3',
         'numpy==1.18.5',
-        'scipy==1.7.3',
+        'scipy==1.5.4',
         'sympy==1.6',
     ],
     'magics' : [
