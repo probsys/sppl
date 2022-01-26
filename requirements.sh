b72f16a61966afb96c1c01a58ea4c200bb25cb54
McCoy Becker <mccoybecker@gmail.com>
2022-01-26 14:12:34 -0500

    Added 'gfortran' as a dependency to grab in requirements.sh.

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Libraries to install pygraphviz from PyPI.
+# Libraries to install pygraphviz from PyPI, as well as build scipy.
 # Tested on Ubuntu 18.04+, other platforms may vary.
 
-apt-get -y install python3-dev graphviz libgraphviz-dev
+apt-get -y install python3-dev graphviz libgraphviz-dev gfortran
